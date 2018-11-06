90cf5cd9214bb966910dc4662b75e67630c97390
Carolyne Peelman <peelmancarolyne@gmail.com>
2018-11-06 12:28:34 +0100

    In memory db

---
+++ src/main/java/be/ucll/da/carey/cityquest/db/GameDb.java
@@ -0,0 +1,11 @@
+package be.ucll.da.carey.cityquest.db;
+
+import be.ucll.da.carey.cityquest.model.Game;
+
+import java.util.ArrayList;
+
+public interface GameDb {
+
+    ArrayList<Game> getAll();
+
+}
---
+++ src/main/java/be/ucll/da/carey/cityquest/db/GameInMemoryDb.java
@@ -0,0 +1,41 @@
+package be.ucll.da.carey.cityquest.db;
+
+import be.ucll.da.carey.cityquest.model.Coordinate;
+import be.ucll.da.carey.cityquest.model.Game;
+import be.ucll.da.carey.cityquest.model.Question;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GameInMemoryDb implements GameDb {
+    private ArrayList<Game> games = new ArrayList<>();
+
+    public GameInMemoryDb() {
+        Question question1 = Question.builder()
+                .question("Hoe heet de kathedraal")
+                .build();
+
+        ArrayList<Question> questionsLeuven = new ArrayList<>();
+        questionsLeuven.add(question1);
+
+        Coordinate coordinateLeuven = Coordinate.builder()
+                .latitude(50.8798438)
+                .longitude(4.700517600000012)
+                .build();
+
+        Game gameLeuven = Game.builder()
+                .name("Leuven City Quest")
+                .city("Leuven")
+                .cityCoordinate(coordinateLeuven)
+                .description("City Quest in Leuven")
+                .questions(questionsLeuven)
+                .build();
+
+        games.add(gameLeuven);
+    }
+
+    @Override
+    public ArrayList<Game> getAll() {
+        return games;
+    }
+}
