8429eeb271c92be65ec0bd5240319c3d749fb7d3
Larry Boltovskoi <larry.bolt@gmail.com>
2018-11-06 10:02:16 +0100

    Initial project setup

---
+++ src/main/java/be/ucll/da/carey/cityquest/CityquestApplication.java
@@ -0,0 +1,12 @@
+package be.ucll.da.carey.cityquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityquestApplication.class, args);
+	}
+}
