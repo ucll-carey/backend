bba1d57a2dab4f49073d9c75435980d5a0639349
Larry Boltovskoi <larry.bolt@gmail.com>
2018-11-11 21:27:27 +0100

    Also send game id

--- src/main/java/be/ucll/da/carey/cityquest/rest/GameWithoutQuestionDTO.java
+++ src/main/java/be/ucll/da/carey/cityquest/rest/GameWithoutQuestionDTO.java
@@ -1,23 +1,16 @@
 package be.ucll.da.carey.cityquest.rest;
 
 import be.ucll.da.carey.cityquest.model.Coordinate;
-import be.ucll.da.carey.cityquest.model.Question;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
-
-import javax.validation.Valid;
-import java.util.ArrayList;
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 public class GameWithoutQuestionDTO {
-    @NonNull
+    private UUID id;
     private String name;
-    @NonNull
     private String description;
-    @NonNull
     private String city;
-    @NonNull @Valid
     private Coordinate cityCoordinate;
 }
